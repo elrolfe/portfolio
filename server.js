33f4d3a4613c47cc330b55f773ea497cd43d155b
Eric <elrolfe@gmail.com>
2016-08-15 18:14:19 +0000

    Added a project template framework

--- server.js
+++ server.js
@@ -3,7 +3,9 @@
 const fs = require("fs");
 
 var express = require("express"),
-    app = express();
+    app = express(),
+    pug = require("pug"),
+    sass = require("node-sass-middleware");
     
 fs.readdir(process.cwd() + "/projects", (err, files) => {
     if (err) throw err;
@@ -11,12 +13,21 @@ fs.readdir(process.cwd() + "/projects", (err, files) => {
     files.forEach((file) => {
         var path = process.cwd() + "/projects/" + file;
         if (fs.statSync(path).isDirectory()) {
-            if (fs.statSync(path + "/subapp.js").isFile()) {
+            try {
+                fs.accessSync(path + "/subapp.js", fs.F_OK);
                 app.use("/" + file, require(path + "/subapp.js"));
-            }
+            } catch(e) {}
         }
-    })
+    });
 });
+
+app.set("views", process.cwd() + "/resources/pug");
+app.set("view engine", "pug");
+
+app.route("/")
+    .get((req, res) => {
+        res.send("Eric's Portfolio (Coming soon)");
+    })
 
 app.listen(process.env.PORT, () => {
     console.log("Portfolio app listening on " + process.env.PORT + "...");
