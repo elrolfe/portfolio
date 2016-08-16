ee3b69869695680c148448dd1586e271db4dc67c
Eric <elrolfe@gmail.com>
2016-08-16 20:37:01 +0000

    Added D3 Scatter Plot project

---
+++ projects/scatter-plot/resources/js/scatter-plot.js
@@ -0,0 +1,537 @@
+const FCC_DATA = [
+{
+"Time": "36:50",
+"Place": 1,
+"Seconds": 2210,
+"Name": "Marco Pantani",
+"Year": 1995,
+"Nationality": "ITA",
+"Doping": "Alleged drug use during 1995 due to high hematocrit levels",
+"URL": "https://en.wikipedia.org/wiki/Marco_Pantani#Alleged_drug_use"
+},
+{
+"Time": "36:55",
+"Place": 2,
+"Seconds": 2215,
+"Name": "Marco Pantani",
+"Year": 1997,
+"Nationality": "ITA",
+"Doping": "Alleged drug use during 1997 due to high hermatocrit levels",
+"URL": "https://en.wikipedia.org/wiki/Marco_Pantani#Alleged_drug_use"
+},
+{
+"Time": "37:15",
+"Place": 3,
+"Seconds": 2235,
+"Name": "Marco Pantani",
+"Year": 1994,
+"Nationality": "ITA",
+"Doping": "Alleged drug use during 1994 due to high hermatocrit levels",
+"URL": "https://en.wikipedia.org/wiki/Marco_Pantani#Alleged_drug_use"
+},
+{
+"Time": "37:36",
+"Place": 4,
+"Seconds": 2256,
+"Name": "Lance Armstrong",
+"Year": 2004,
+"Nationality": "USA",
+"Doping": "2004 Tour de France title stripped by UCI in 2012",
+"URL": "https://en.wikipedia.org/wiki/History_of_Lance_Armstrong_doping_allegations"
+},
+{
+"Time": "37:42",
+"Place": 5,
+"Seconds": 2262,
+"Name": "Jan Ullrich",
+"Year": 1997,
+"Nationality": "GER",
+"Doping": "Confessed later in his career to doping",
+"URL": "https://en.wikipedia.org/wiki/Jan_Ullrich#Operaci.C3.B3n_Puerto_doping_case"
+},
+{
+"Time": "38:05",
+"Place": 6,
+"Seconds": 2285,
+"Name": "Lance Armstrong",
+"Year": 2001,
+"Nationality": "USA",
+"Doping": "2001 Tour de France title stripped by UCI in 2012",
+"URL": "https://en.wikipedia.org/wiki/History_of_Lance_Armstrong_doping_allegations"
+},
+{
+"Time": "38:14",
+"Place": 7,
+"Seconds": 2294,
+"Name": "Miguel Indurain",
+"Year": 1995,
+"Nationality": "ESP",
+"Doping": "1994 Failed test for salbutemol, not a banned drug at that time",
+"URL": "http://www.independent.co.uk/sport/drugs-in-sport-indurain-allowed-to-use-banned-drug-1379584.html"
+},
+{
+"Time": "38:14",
+"Place": 8,
+"Seconds": 2294,
+"Name": "Alex Zülle",
+"Year": 1995,
+"Nationality": "SUI",
+"Doping": "Confessed in 1998 to taking EPO",
+"URL": "https://en.wikipedia.org/wiki/Alex_Z%C3%BClle#Festina_affair"
+},
+{
+"Time": "38:16",
+"Place": 9,
+"Seconds": 2296,
+"Name": "Bjarne Riis",
+"Year": 1995,
+"Nationality": "DEN",
+"Doping": "Alleged drug use during 1995 due to high hermatrocite levels",
+"URL": "https://en.wikipedia.org/wiki/Bjarne_Riis#Doping_allegations"
+},
+{
+"Time": "38:22",
+"Place": 10,
+"Seconds": 2302,
+"Name": "Richard Virenque",
+"Year": 1997,
+"Nationality": "FRA",
+"Doping": "In 2000 confessed to doping during his career",
+"URL": "https://en.wikipedia.org/wiki/Richard_Virenque#Festina_affair"
+},
+{
+"Time": "38:36",
+"Place": 11,
+"Seconds": 2316,
+"Name": "Floyd Landis",
+"Year": 2006,
+"Nationality": "USA",
+"Doping": "Stripped of 2006 Tour de France title",
+"URL": "https://en.wikipedia.org/wiki/Floyd_Landis_doping_case"
+},
+{
+"Time": "38:36",
+"Place": 12,
+"Seconds": 2316,
+"Name": "Andreas Klöden",
+"Year": 2006,
+"Nationality": "GER",
+"Doping": "Alleged use of illegal blood transfusions in 2006",
+"URL": "https://en.wikipedia.org/wiki/Andreas_Kl%C3%B6den#2009_Doping_allegations"
+},
+{
+"Time": "38:40",
+"Place": 13,
+"Seconds": 2320,
+"Name": "Jan Ullrich",
+"Year": 2004,
+"Nationality": "GER",
+"Doping": "Confessed later in his career to doping",
+"URL": "https://en.wikipedia.org/wiki/Jan_Ullrich#Operaci.C3.B3n_Puerto_doping_case"
+},
+{
+"Time": "38:44",
+"Place": 14,
+"Seconds": 2324,
+"Name": "Laurent Madouas",
+"Year": 1995,
+"Nationality": "FRA",
+"Doping": "Tested positive for Salbutemol in 1994, suspended for 1 month",
+"URL": "http://www.dopeology.org/incidents/Madouas-positive/"
+},
+{
+"Time": "38:55",
+"Place": 15,
+"Seconds": 2335,
+"Name": "Richard Virenque",
+"Year": 1994,
+"Nationality": "FRA",
+"Doping": "In 2000 confessed to doping during his career",
+"URL": "https://en.wikipedia.org/wiki/Richard_Virenque#Festina_affair"
+},
+{
+"Time": "39:01",
+"Place": 16,
+"Seconds": 2341,
+"Name": "Carlos Sastre",
+"Year": 2006,
+"Nationality": "ESP",
+"Doping": "",
+"URL": ""
+},
+{
+"Time": "39:09",
+"Place": 17,
+"Seconds": 2349,
+"Name": "Iban Mayo",
+"Year": 2003,
+"Nationality": "ESP",
+"Doping": "Failed doping test in 2007 Tour de France",
+"URL": "https://en.wikipedia.org/wiki/Iban_Mayo"
+},
+{
+"Time": "39:12",
+"Place": 18,
+"Seconds": 2352,
+"Name": "Andreas Klöden",
+"Year": 2004,
+"Nationality": "GER",
+"Doping": "Alleged doping during 2006 Tour de France",
+"URL": "https://en.wikipedia.org/wiki/Operaci%C3%B3n_Puerto_doping_case"
+},
+{
+"Time": "39:14",
+"Place": 19,
+"Seconds": 2354,
+"Name": "Jose Azevedo",
+"Year": 2004,
+"Nationality": "POR",
+"Doping": "Implicated in the Operación Puerto doping case",
+"URL": "https://en.wikipedia.org/wiki/Operaci%C3%B3n_Puerto_doping_case"
+},
+{
+"Time": "39:15",
+"Place": 20,
+"Seconds": 2355,
+"Name": "Levi Leipheimer",
+"Year": 2006,
+"Nationality": "USA",
+"Doping": "Testified in 2012 to doping during his time with US Postal Service ",
+"URL": "http://www.wsj.com/articles/SB10000872396390444799904578048352672452328"
+},
+{
+"Time": "39:22",
+"Place": 21,
+"Seconds": 2362,
+"Name": "Francesco Casagrande",
+"Year": 1997,
+"Nationality": "ITA",
+"Doping": "Positive testosterone test in 1998",
+"URL": "http://autobus.cyclingnews.com/results/1998/sep98/sep2.shtml"
+},
+{
+"Time": "39:23",
+"Place": 22,
+"Seconds": 2363,
+"Name": "Nairo Quintana",
+"Year": 2015,
+"Nationality": "COL",
+"Doping": "",
+"URL": ""
+},
+{
+"Time": "39:23",
+"Place": 23,
+"Seconds": 2363,
+"Name": "Bjarne Riis",
+"Year": 1997,
+"Nationality": "DEN",
+"Doping": "Alleged drug use during 1995 due to high hermatrocite levels",
+"URL": "https://en.wikipedia.org/wiki/Bjarne_Riis#Doping_allegations"
+},
+{
+"Time": "39:30",
+"Place": 24,
+"Seconds": 2370,
+"Name": "Miguel Indurain",
+"Year": 1994,
+"Nationality": "ESP",
+"Doping": "1994 Failed test for salbutemol, not a banned drug at that time",
+"URL": "http://www.independent.co.uk/sport/drugs-in-sport-indurain-allowed-to-use-banned-drug-1379584.html"
+},
+{
+"Time": "39:30",
+"Place": 25,
+"Seconds": 2370,
+"Name": "Luc Leblanc",
+"Year": 1994,
+"Nationality": "FRA",
+"Doping": "Admitted to using epo and amphetimines throughout 1994 ",
+"URL": "http://www.dopeology.org/people/Luc_Leblanc/"
+},
+{
+"Time": "39:32",
+"Place": 26,
+"Seconds": 2372,
+"Name": "Carlos Sastre",
+"Year": 2008,
+"Nationality": "ESP",
+"Doping": "",
+"URL": ""
+},
+{
+"Time": "39:37",
+"Place": 27,
+"Seconds": 2377,
+"Name": "Vladimir Poulnikov",
+"Year": 1994,
+"Nationality": "UKR",
+"Doping": "",
+"URL": ""
+},
+{
+"Time": "39:40",
+"Place": 28,
+"Seconds": 2380,
+"Name": "Giuseppe Guerini",
+"Year": 2004,
+"Nationality": "ITA",
+"Doping": "",
+"URL": ""
+},
+{
+"Time": "39:41",
+"Place": 29,
+"Seconds": 2381,
+"Name": "Santos Gonzalez",
+"Year": 2004,
+"Nationality": "ESP",
+"Doping": "High Hematocrit during 2005 season, removed by team management",
+"URL": "http://www.cyclingnews.com/news/santos-gonzalez-sacked-by-phonak/"
+},
+{
+"Time": "39:41",
+"Place": 30,
+"Seconds": 2381,
+"Name": "Vladimir Karpets",
+"Year": 2004,
+"Nationality": "RUS",
+"Doping": "Made payments to Ferrari, but no charges filed",
+"URL": "http://www.dopeology.org/incidents/Ferrari-investigation/"
+},
+{
+"Time": "39:45",
+"Place": 31,
+"Seconds": 2385,
+"Name": "Fernando Escartin",
+"Year": 1995,
+"Nationality": "ESP",
+"Doping": "Implicated in Giardini Margherita Raid in 1998 as a 'victim' ",
+"URL": "http://www.dopeology.org/incidents/Giardini-Margherita-raid-%5bBologna%5d/"
+},
+{
+"Time": "39:47",
+"Place": 32,
+"Seconds": 2387,
+"Name": "Denis Menchov",
+"Year": 2006,
+"Nationality": "RUS",
+"Doping": "",
+"URL": ""
+},
+{
+"Time": "39:47",
+"Place": 33,
+"Seconds": 2387,
+"Name": "Michael Rasmussen",
+"Year": 2006,
+"Nationality": "DEN",
+"Doping": "Admitted to doping with multiple substances 1998-2010",
+"URL": "http://www.dopeology.org/people/Michael_Rasmussen/"
+},
+{
+"Time": "39:47",
+"Place": 34,
+"Seconds": 2387,
+"Name": "Pietro Caucchioli",
+"Year": 2006,
+"Nationality": "ITA",
+"Doping": "Associated with Mantova investigation, charges dropped",
+"URL": "http://www.cyclingnews.com/news/italian-judge-set-to-decide-if-32-named-in-mantova-doping-investigation-should-go-on-trial/"
+},
+{
+"Time": "39:50",
+"Place": 35,
+"Seconds": 2390,
+"Name": "Nairo Quintana",
+"Year": 2013,
+"Nationality": "COL",
+"Doping": "",
+"URL": ""
+}
+];
+
+var dataAdjustment = d3.min(FCC_DATA, function(d) { return d.Seconds; });
+
+var margin = {top: 150, right: 110, bottom: 60, left: 60},
+    width = 1000 - margin.right - margin.left,
+    height = 760 - margin.top - margin.bottom;
+
+var x = d3.time.scale()
+   .domain([d3.min(FCC_DATA, function(d) { return new Date(0, 0, 0, 0, 0, d.Seconds - dataAdjustment); }),
+            d3.max(FCC_DATA, function(d) { return new Date(0, 0, 0, 0, 0, d.Seconds - dataAdjustment); })])
+   .range([width, 0]);
+
+var xAxis = d3.svg.axis()
+   .scale(x)
+   .orient("bottom");
+
+var y = d3.scale.linear()
+   .domain([d3.min(FCC_DATA, function(d) { return d.Place; }),
+            d3.max(FCC_DATA, function(d) { return d.Place; })])
+   .range([0, height]);
+
+var yAxis = d3.svg.axis()
+   .scale(y)
+   .orient("left");
+
+var defs = d3.select(".chart")
+   .append("defs")
+   .append("filter")
+   .attr("id", "drop-shadow")
+   .attr("x", 0)
+   .attr("y", 0)
+   .attr("width", "200%")
+   .attr("height", "200%")
+
+defs.append("feOffset")
+   .attr("result", "offOut")
+   .attr("in", "SourceGraphic")
+   .attr("dx", 5)
+   .attr("dy", 5);
+
+defs.append("feGaussianBlur")
+   .attr("result", "blurOut")
+   .attr("in", "offOut")
+   .attr("stdDeviation", 3);
+
+defs.append("feBlend")
+   .attr("in", "SourceGraphic")
+   .attr("in2", "blurOut")
+   .attr("mode", "normal");
+
+d3.select(".chart")
+   .append("rect")
+   .attr("x", 0)
+   .attr("y", 0)
+   .attr("width", width + margin.left + margin.right)
+   .attr("height", height + margin.top + margin.bottom)
+   .style("fill", "rgba(255, 255, 255, .85)");
+
+d3.select(".chart")
+   .append("text")
+   .attr("class", "title")
+   .attr("x", (width + margin.left + margin.right) / 2)
+   .attr("y", 40)
+   .attr("filter", "url(#drop-shadow)")
+   .text("Doping in Professional Bicycle Racing");
+
+d3.select(".chart")
+   .append("text")
+   .attr("class", "subtitle")
+   .attr("x", (width + margin.left + margin.right) / 2)
+   .attr("y", 80)
+   .text("35 Fastest times up Alpe d'Huez");
+
+d3.select(".chart")
+   .append("text")
+   .attr("class", "subtitle")
+   .attr("x", (width + margin.left + margin.right) / 2)
+   .attr("y", 100)
+   .text("(Normalized to 13.8km distance)");
+
+var chart = d3.select(".chart")
+   .attr("width", width + margin.left + margin.right)
+   .attr("height", height + margin.top + margin.bottom)
+   .append("g")
+   .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
+
+
+chart.selectAll("circle")
+   .data(FCC_DATA)
+   .enter().append("circle")
+   .attr("class", function(d) { return d.Doping == "" ? "clean" : "doping"; })
+   .attr("cx", function(d) { return x(new Date(0, 0, 0, 0, 0, d.Seconds - dataAdjustment)); })
+   .attr("cy", function(d) { return y(d.Place); })
+   .attr("r", 5)
+   .on("mouseover", function(d) {
+      d3.select(".cyclist-name").text(d.Name);
+      d3.select(".nationality").text(d.Nationality);
+      d3.select(".year").text(d.Year);
+      d3.select(".time").text(d.Time);
+      if (d.Doping == "") {
+         d3.select(".allegation").text("No doping allegations");
+      } else {
+         d3.select(".allegation").text(d.Doping);
+      }
+      d3.select(".tooltip")
+         .style("opacity", 1)
+         .style("top", (d3.event.pageY - 22) + "px")
+         .style("left", (d3.event.pageX - 240) + "px")
+   })
+   .on("mouseout", function() {
+      d3.select(".tooltip")
+         .style("opacity", 0)
+         .style("top", "-100px")
+         .style("left", "-100px")
+   });
+
+// chart.selectAll("text")
+//    .data(FCC_DATA)
+//    .enter().append("text")
+//    .text(function(d) { return d.Name; })
+//    .attr("x", function(d) { return x(new Date(0, 0, 0, 0, 0, d.Seconds - dataAdjustment)) + 8; })
+//    .attr("y", function(d) { return y(d.Place) + 5; });
+
+chart.selectAll("a")
+   .data(FCC_DATA)
+   .enter().append("a")
+   .attr("class", function(d) { return (d.URL == "" ? "inactive-link" : "active-link"); })
+   .attr("xlink:href", function(d) { return (d.URL == "" ? "#" : d.URL); })
+   .attr("target", "_blank")
+   .append("text")
+   .text(function(d) { return d.Name; })
+   .attr("x", function(d) { return x(new Date(0, 0, 0, 0, 0, d.Seconds - dataAdjustment)) + 8; })
+   .attr("y", function(d) { return y(d.Place) + 5; });
+
+var legend = chart.append("g")
+   .attr("class", "legend");
+
+legend.append("rect")
+   .attr("width", 180)
+   .attr("height", 50)
+   .attr("x", 20)
+   .attr("y", 20);
+
+legend.append("circle")
+   .attr("class", "doping")
+   .attr("cx", 35)
+   .attr("cy", 35)
+   .attr("r", 5);
+
+legend.append("text")
+   .attr("x", 45)
+   .attr("y", 40)
+   .text("Doping Allegations Made");
+
+legend.append("circle")
+   .attr("class", "clean")
+   .attr("cx", 35)
+   .attr("cy", 55)
+   .attr("r", 5);
+
+legend.append("text")
+   .attr("x", 45)
+   .attr("y", 60)
+   .text("No Doping Allegations");
+
+chart.append("g")
+   .attr("class", "y axis")
+   .attr("transform", "translate(-10, 0)")
+   .call(yAxis)
+   .append("text")
+   .attr("transform", "rotate(-90), translate(0, -40)")
+   .attr("y", 6)
+   .attr("dy", ".71em")
+   .style("text-anchor", "end")
+   .text("Ranking");
+
+chart.append("g")
+   .attr("class", "x axis")
+   .attr("transform", "translate(0, " + (height + 10) + ")")
+   .call(xAxis.tickFormat(d3.time.format("%M:%S")).ticks(d3.time.second, 15))
+   .append("text")
+   .attr("x", width)
+   .attr("y", 35)
+   .style("text-anchor", "end")
+   .text("Minutes behind the fastest time");
