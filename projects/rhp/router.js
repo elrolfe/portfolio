02bf9e02c81388d513f41165c719c4256876de02
Eric <elrolfe@gmail.com>
2016-08-17 16:26:39 +0000

    Added Request Header Parser Microservice project

---
+++ projects/rhp/router.js
@@ -0,0 +1,23 @@
+"use strict";
+
+module.exports = function(app) {
+    app.route("/")
+        .get((req, res) => {
+            res.render("index", {
+                protocol: req.headers["x-forwarded-proto"],
+                host: req.headers.host,
+                base: req.headers.baseUrl
+            });
+        });
+        
+    app.route("/api")
+        .get((req, res) => {
+            var obj = {
+                ipaddress: req.headers["x-forwarded-for"],
+                language: req.headers["accept-language"].split(',')[0],
+                software: req.headers["user-agent"].split('(')[1].split(')')[0]
+            };
+            
+            res.send(obj);
+        });
+};
---
+++ projects/rhp/subapp.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const baseDir = process.cwd() + "/projects/rhp";
+
+var express = require("express"),
+    subapp = express(),
+    pug = require("pug"),
+    sass = require("node-sass-middleware"),
+    router = require(baseDir + "/router.js");
+    
+subapp.set("views", baseDir + "/resources/pug");
+subapp.set("view engine", "pug");
+
+subapp.use(sass({
+    src: baseDir + "/resources/sass",
+    dest: baseDir + "/resources/css",
+    prefix: "/css",
+    output: "compressed"
+}));
+
+subapp.use("/css", express.static(baseDir + "/resources/css"));
+
+router(subapp);
+
+module.exports = subapp;
